c7fd6f982c8b2d39abe91e06ed7273586304f64f
illshaadVinidaily <illshaad@vinidaily.com>
2023-06-06 22:35:43 +0200

    change file cv to dist

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ export default function Button() {
     <div className="flex justify-center">
       <a
         className="inline-block rounded bg-gradient-to-r from-[#5271FF] to-[#8C52FF] p-[2px] hover:text-white focus:outline-none focus:ring active:text-opacity-75"
-        href="../public/illshaad.pdf"
+        href="../dist/illshaad.pdf"
         download
       >
         <span className="block rounded-sm font-bold bg-white px-4 py-3 w-[155px] text-center text-sm font-medium hover:bg-transparent">
