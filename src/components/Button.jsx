5706b622a36d54fca59bd64bdec6020dcda3db08
illshaadVinidaily <illshaad@vinidaily.com>
2023-06-07 09:11:50 +0200

    remove balise prev

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,12 @@
 export default function Button() {
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center sm: pb-4">
       <a
         className="inline-block rounded bg-gradient-to-r  from-[#5271FF] to-[#8C52FF] p-[2px] hover:text-white focus:outline-none focus:ring active:text-opacity-75"
-        href="../dist/illshaad.pdf"
+        href="https://drive.google.com/file/d/12yoDc3SPwmByZ-Jg63t2cC7gL1cCsyf9/view?usp=sharing"
         download
       >
-        <span className="block rounded-sm font-bold bg-white py-3 w-[155px] text-center text-sm font-medium hover:bg-transparent  sm:text-xs w-[120px]">
+        <span className="block rounded-sm font-bold bg-white py-3 w-[155px] text-center text-sm font-medium hover:bg-transparent  sm: text-xs w-[10px]">
           Telecharger le cv
         </span>
       </a>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,62 +5,62 @@ import Button from "./Button";
 export default function Home() {
   return (
     <div className="md:flex-row sm: flex flex-col items-center justify-center h-screen">
-      <div className="animate__animated animate__fadeInLeft bg-slate-50 h-full w-full flex flex-col items-center justify-center sm:pb-6">
+      <div className="animate__animated animate__fadeInLeft bg-slate-50  w-full flex flex-col items-center justify-center sm:pb-6">
         <img src={Logo} alt="logo" width={250} />
-        <p className="text-center px-4 pb-6 italic font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#5271FF] to-[#8C52FF]">
+        <p className=" text-center px-4 pb-6 italic font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#5271FF] to-[#8C52FF]  sm: text-sm ">
           En tant que développeur web full-stack, j'utilise React, React-Native,
           Next.js et Node.js pour créer des applications performantes et
           interactives.
         </p>
         <Button />
       </div>
-      <div className="bg-sky-950 h-full px-4 py-4 flex items-center justify-center">
+      <div className="bg-sky-950 h-full px-4 py-4 flex items-center justify-center sm: h-screen">
         <div>
-          <code className="font-bold text-[#8C52FF] sm:text-sm">
+          <code className="font-bold text-[#8C52FF] sm: text-sm">
             Présentation : {"{"}
           </code>
           {Object.entries(presentation).map(([key, value], index) => (
-            <pre key={index}>
+            <div key={index}>
               &nbsp;&nbsp;&nbsp;&nbsp;
-              <code className="text-green-600 sm:text-sm">{key}</code>:{" "}
-              <code className="text-blue-600 sm:text-sm">"{value}",</code>
-            </pre>
+              <code className="text-green-600 sm: text-sm">{key}</code>:{" "}
+              <code className="text-blue-600 sm: text-sm">"{value}",</code>
+            </div>
           ))}
           <code className="font-bold text-[#8C52FF]">{"}"},</code>
           <br />
           <br />
-          <code className="font-bold text-[#8C52FF]  sm:text-sm">
+          <code className="font-bold text-[#8C52FF]  sm: text-sm">
             Experience : {"{"}{" "}
           </code>
           {experiences.map((experience, index) => (
-            <pre key={index}>
+            <div key={index}>
               &nbsp;&nbsp;&nbsp;&nbsp;
-              <code className="text-green-600  sm:text-sm">
+              <code className="text-green-600  sm: text-sm">
                 {experience.year}
               </code>
               :{" "}
-              <code className="text-blue-600  sm:text-sm">
+              <code className="text-blue-600  sm: text-sm">
                 "{experience.role} @ {experience.company}",
               </code>
-            </pre>
+            </div>
           ))}
           <code className="font-bold text-[#8C52FF]">{"}"},</code>
           <br />
           <br />
-          <code className="font-bold text-[#8C52FF]  sm:text-sm">
+          <code className="font-bold text-[#8C52FF]  sm: text-sm">
             Projets : {"{"}{" "}
           </code>
           {Object.entries(projets).map(([key, value], index) => (
-            <pre key={index}>
+            <div key={index}>
               &nbsp;&nbsp;&nbsp;&nbsp;
-              <code className="text-green-600  sm:text-sm">{key}</code>:{" "}
-              <code className="text-blue-600  sm:text-sm ">
+              <code className="text-green-600  sm: text-sm">{key}</code>:{" "}
+              <code className="text-blue-600  sm: text-sm ">
                 <a href={value} target="_blank" rel="noreferrer">
                   "{value}"
                 </a>
                 ,
               </code>
-            </pre>
+            </div>
           ))}
           <code className="font-bold text-[#8C52FF]">{"}"}</code>
         </div>
